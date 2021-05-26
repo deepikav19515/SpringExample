7b07106067cb3a10b07e6f82df333b542388d690
deepika <deepika.v@sonata-software.com>
2021-05-26 15:46:04 +0530

    First API Rest Project

---
+++ src/main/java/com/sonata/Repository/EmployeeRepository.java
@@ -0,0 +1,13 @@
+package com.sonata.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.sonata.Model.Employee;
+
+
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long>{
+
+}
